90fe8d911d5859bb4c759d638f652c46012fbf68
Latishev Andrey <andrash.late@gmail.com>
2014-08-05 20:14:46 +0700

    TEST = -TOAST

--- TimeFly/src/timefly/activity/CalenderActivity.java
+++ TimeFly/src/timefly/activity/CalenderActivity.java
@@ -13,7 +13,6 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.CalendarView.OnDateChangeListener;
-import android.widget.Toast;
 
 public class CalenderActivity extends Activity {
 
